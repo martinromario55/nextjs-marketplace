347851e613fb2a38afb1f0037fb6c1b4e3b15c7c
martinromario55 <martinromario55@gmail.com>
2024-06-18 23:40:33 +0300

    Home Page setup

---
+++ src/app/_components/ProductCard.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
+import Link from "next/link";
+
+interface iAppProps {
+  images: string[];
+  name: string;
+  price: number;
+  smallDescription: string;
+  id: string;
+}
+
+export function ProductCard({
+  images,
+  id,
+  price,
+  smallDescription,
+  name,
+}: iAppProps) {
+  return (
+    <div className="rounded-lg">
+      <Carousel className="mx-auto w-full">
+        <CarouselContent>
+          {images.map((item, index) => (
+            <CarouselItem key={index}>
+              <div className="relative h-[230px]">
+                <Image
+                  alt="Product image"
+                  src={item}
+                  fill
+                  className="h-full w-full rounded-lg object-cover"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="ml-16" />
+        <CarouselNext className="mr-16" />
+      </Carousel>
+
+      <div className="mt-2 flex items-center justify-between">
+        <h1 className="text-xl font-semibold">{name}</h1>
+        <h3 className="inline-flex items-center rounded-md bg-primary/10 px-2 py-1 text-xs font-medium text-primary ring-1 ring-inset ring-primary/10">
+          ${price}
+        </h3>
+      </div>
+
+      <p className="mt-2 line-clamp-2 text-sm text-gray-600">
+        {smallDescription}
+      </p>
+
+      <Button asChild className="mt-5 w-full">
+        <Link href={`/product/${id}`}>Learn More!</Link>
+      </Button>
+    </div>
+  );
+}
+
+export function LoadingProductCard() {
+  return (
+    <div className="flex flex-col">
+      <Skeleton className="h-[230px] w-full" />
+      <div className="mt-2 flex flex-col gap-y-2">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-6 w-full" />
+      </div>
+
+      <Skeleton className="mt-5 h-10 w-full" />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import { NewestProducts } from "./_components/NewestProducts";
+
 const page = () => {
-  return <div>Home page</div>;
+  return (
+    <section className="mx-auto mb-24 max-w-7xl px-4 md:px-8">
+      <div className="mx-auto max-w-3xl text-center text-2xl font-semibold sm:text-5xl lg:text-6xl">
+        <h1>Find the best Tailwind</h1>
+        <h1 className="text-primary">Templates & Icons</h1>
+        <p className="mx-auto mt-5 w-[90%] text-base font-normal text-muted-foreground lg:text-lg">
+          MarshalUi stands out as the premier marketplace for all things related
+          to tailwindcss, offering an unparalleled platform for both sellers and
+          buyers alike.
+        </p>
+      </div>
+      <NewestProducts />
+      {/* <ProductRow category="newest" />
+      <ProductRow category="templates" />
+      <ProductRow category="icons" />
+      <ProductRow category="uikits" /> */}
+    </section>
+  );
 };
 
 export default page;
