bdc0b049396e95a0f2d3b148a7f49b93167b3fde
martinromario55 <martinromario55@gmail.com>
2024-06-18 23:14:13 +0300

    Settings Page setup

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -29,6 +29,20 @@ const productSchema = z.object({
     .min(1, { message: "Pleaes upload a zip of your product" }),
 });
 
+const userSettingsSchema = z.object({
+  firstName: z
+    .string()
+    .min(3, { message: "Minimum length of 3 required" })
+    .or(z.literal(""))
+    .optional(),
+
+  lastName: z
+    .string()
+    .min(3, { message: "Minimum length of 3 required" })
+    .or(z.literal(""))
+    .optional(),
+});
+
 export async function SellProduct(prevState: any, formData: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
@@ -74,6 +88,47 @@ export async function SellProduct(prevState: any, formData: FormData) {
   const state: State = {
     status: "success",
     message: "Your product has been uploaded",
+  };
+
+  return state;
+}
+
+export async function UpdateUserSettings(prevState: any, formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("something went wrong");
+  }
+
+  const validateFields = userSettingsSchema.safeParse({
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+  });
+
+  if (!validateFields.success) {
+    const state: State = {
+      status: "error",
+      errors: validateFields.error.flatten().fieldErrors,
+      message: "Oops, I think there is a mistake with your inputs.",
+    };
+
+    return state;
+  }
+
+  const data = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      firstName: validateFields.data.firstName,
+      lastName: validateFields.data.lastName,
+    },
+  });
+
+  const state: State = {
+    status: "success",
+    message: "Your Settings have been updated",
   };
 
   return state;
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,42 @@
+import { Card } from "@/components/ui/card";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { SettingsForm } from "../_components/SettingsForm";
+import { unstable_noStore as noStore } from "next/cache";
+import prisma from "../_lib/db";
+
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      firstName: true,
+      lastName: true,
+      email: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function SetttingsPage() {
+  noStore();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    throw new Error("Not Authorized");
+  }
+
+  const data = await getData(user.id);
+  return (
+    <section className="mx-auto max-w-7xl px-4 md:px-8">
+      <Card>
+        <SettingsForm
+          firstName={data?.firstName as string}
+          lastName={data?.lastName as string}
+          email={data?.email as string}
+        />
+      </Card>
+    </section>
+  );
+}
